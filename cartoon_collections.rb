0bf944f7ded48b6c1beb1588da5050ba860119e0
Annia Forney <anniaforney@gmail.com>
2019-04-20 21:21:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,7 @@
 def roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]
 end
 
-def summon_captain_planet# code an argument here
-  planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_plane planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
 end
 
 def long_planeteer_calls# code an argument here
