199cb43f7b5ed7192619e77125149ae7e0868b51
Annia Forney <anniaforney@gmail.com>
2019-04-20 21:50:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 
 def summon_captain_planet # code an argument here 
 planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
-planeteer_calls.collect {|x| 
+planeteer_calls.collect {|x| x + 
 end
 
 def long_planeteer_calls# code an argument here
