38894e7cb516d35e6107463215b1e75ae89c2486
Annia Forney <anniaforney@gmail.com>
2019-04-20 21:56:11 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,11 @@ planeteer_calls #=> ["earth", "wind", "fire", "water", "heart"]
 end
 
 def long_planeteer_calls# code an argument here
-
+short_words = ["puff", "go", "two"]
+long_planeteer_calls(short_words)
+ 
+assorted_words = ["two", "go", "industrious", "bop"]
+long_planeteer_calls(assorted_words)
 end
 
 def find_the_cheese# code an argument here
