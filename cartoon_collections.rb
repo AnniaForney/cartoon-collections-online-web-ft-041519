717b5226dfc28ba9195c1c51809f708f9958cd9e
Annia Forney <anniaforney@gmail.com>
2019-04-20 21:22:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,17 +1,13 @@
-def roll_call_dwarves ["Doc", "Dopey", "Bashful", "Grumpy"]
+def roll_call_dwarves 
 end
 
-def summon_captain_plane planeteer_calls = ["earth", "wind", "fire", "water", "heart"]
+def summon_captain_plane 
 end
 
 def long_planeteer_calls# code an argument here
-  hort_words = ["puff", "go", "two"]
- 
-assorted_words = ["two", "go", "industrious", "bop"]
-long_planeteer_calls(assorted_words)
+
 end
 
 def find_the_cheese# code an argument here
   # the array below is here to help
-  cheese_types = ["cheddar", "gouda", "camembert"]
 end
